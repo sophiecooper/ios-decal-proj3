bd0e5d6ecf43ea3cde2ab6c114dac63cc1ff2f00
Sophie Cooper <sophie@cooperzone.net>
2016-04-12 12:30:26 -0700

    almost done

--- Photos/Photos/PhotoDetailViewController.swift
+++ Photos/Photos/PhotoDetailViewController.swift
@@ -13,17 +13,31 @@ class PhotoDetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var imageUsername: UILabel?;
     @IBOutlet var imageLikes: UILabel?;
+    @IBOutlet var likeButton: UIButton?;
     
     var selectedPhoto: Array<String>!
     
+    func likePressed() {
+        if (likeButton!.titleLabel!.text! == "Like") {
+            imageLikes!.text = String(Int(imageLikes!.text!)!+1);
+            likeButton!.setTitle("Unlike", forState: UIControlState.Normal)
+            
+        } else {
+            imageLikes!.text = String(Int(imageLikes!.text!)!-1);
+            likeButton!.setTitle("Like", forState: UIControlState.Normal)
+        }
+    }
+    
     override func viewDidLoad() {
-        print(selectedPhoto)
-        
+        likeButton!.setTitle("Like", forState: UIControlState.Normal)
         let url = NSURL(string: selectedPhoto[0]);
         let photoData = NSData(contentsOfURL: url!);
         imageView.image = UIImage(data: photoData!);
         
         imageUsername!.text = selectedPhoto[1];
         imageLikes!.text = selectedPhoto[2];
+        
+        likeButton!.addTarget(self, action: "likePressed", forControlEvents: .TouchUpInside)
     }
+    
 }
